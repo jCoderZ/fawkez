886747eb429039d93adfcdea49c220c36b810236
mgriffel <mchael.griffel@gmail.com>
2011-07-23 21:10:51 +0000

    fixed "mvn clean install" and "mvn eclipse:clean eclipse:eclipse" builds
    git-svn-id: http://www.jcoderz.org/svn/fawkez/trunk-ng@1710 45de2fb7-1323-0410-9dde-b99127fcd57e

--- fawkez-codegen/src/main/java/org/jcoderz/commons/maven/AppInfoMojo.java
+++ fawkez-codegen/src/main/java/org/jcoderz/commons/maven/AppInfoMojo.java
@@ -20,66 +20,67 @@ import org.jcoderz.commons.util.XsltBase;
  * 
  */
 public class AppInfoMojo extends AbstractMojo {
-	/**
-	 * <i>Maven Internal</i>: Project to interact with.
-	 * 
-	 * @parameter default-value="${project}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenProject project;
+        /**
+         * <i>Maven Internal</i>: Project to interact with.
+         * 
+         * @parameter default-value="${project}"
+         * @required
+         * @readonly
+         */
+        private MavenProject project;
 
-	/**
-	 * The XSL stylesheet file.
-	 * 
-	 * @parameter default-value= "generate-log-message-info.xsl"
-	 */
-	private String xslFile = null;
+        /**
+         * The XSL stylesheet file.
+         * 
+         * @parameter default-value= "generate-log-message-info.xsl"
+         */
+        private String xslFile = null;
 
-	/**
-	 * An include pattern for the simple type definition files.
-	 * 
-	 * @parameter default-value="app.info.xml"
-	 */
-	private String includePattern;
+        /**
+         * An include pattern for the simple type definition files.
+         * 
+         * @parameter default-value="app.info.xml"
+         */
+        private String includePattern;
 
-	/**
-	 * The source folder with the simple type XML definitions.
-	 * 
-	 * @parameter
-	 */
-	private File sourceDirectory;
+        /**
+         * The source folder with the simple type XML definitions.
+         * 
+         * @parameter
+         */
+        private File sourceDirectory;
 
-	/**
-	 * The destination folder where the generated code will be written to.
-	 * 
-	 * @parameter
-	 */
-	private File destinationDirectory;
+        /**
+         * The destination folder where the generated code will be written to.
+         * 
+         * @parameter
+         */
+        private File destinationDirectory;
 
-	// TODO:
-	// http://code.hammerpig.com/search-for-files-in-directory-using-wildcards-in-java.html
-	public void execute() throws MojoExecutionException {
-		if (!destinationDirectory.exists())
-		{
-			destinationDirectory.mkdirs();
-		}
-		List<File> files = findFiles(sourceDirectory, includePattern);
-		for (File file : files) {
-			String log = file.getName() + ".log";
-			getLog().info("" + file.getName() + " -> " + destinationDirectory.getName());
-			XsltBase.transform(file, xslFile, destinationDirectory, new File(project
-					.getBuild().getDirectory(), log), false);
-		}
-	}
+        // TODO:
+        // http://code.hammerpig.com/search-for-files-in-directory-using-wildcards-in-java.html
+        public void execute() throws MojoExecutionException {
+                if (!destinationDirectory.exists())
+                {
+                        destinationDirectory.mkdirs();
+                }
+                List<File> files = findFiles(sourceDirectory, includePattern);
+                for (File file : files) {
+                        String log = file.getName() + ".log";
+                        getLog().info("" + file.getName() + " -> " + destinationDirectory.getName());
+                        XsltBase.transform(file, xslFile, destinationDirectory, new File(project
+                                        .getBuild().getDirectory(), log), false);
+                }
+                project.addCompileSourceRoot(destinationDirectory.getAbsolutePath());
+        }
 
-	private List<File> findFiles(File dir, String pattern) {
-		List<File> files = new ArrayList<File>();
-		for (File file : dir.listFiles()) {
-			if (file.getName().endsWith((pattern))) {
-				files.add(file);
-			}
-		}
-		return files;
-	}
+        private List<File> findFiles(File dir, String pattern) {
+                List<File> files = new ArrayList<File>();
+                for (File file : dir.listFiles()) {
+                        if (file.getName().endsWith((pattern))) {
+                                files.add(file);
+                        }
+                }
+                return files;
+        }
 }
--- fawkez-codegen/src/main/java/org/jcoderz/commons/maven/SimpleTypesMojo.java
+++ fawkez-codegen/src/main/java/org/jcoderz/commons/maven/SimpleTypesMojo.java
@@ -20,65 +20,66 @@ import org.jcoderz.commons.util.XsltBase;
  * 
  */
 public class SimpleTypesMojo extends AbstractMojo {
-	/**
-	 * <i>Maven Internal</i>: Project to interact with.
-	 * 
-	 * @parameter default-value="${project}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenProject project;
+        /**
+         * <i>Maven Internal</i>: Project to interact with.
+         * 
+         * @parameter default-value="${project}"
+         * @required
+         * @readonly
+         */
+        private MavenProject project;
 
-	/**
-	 * The XSL stylesheet file.
-	 * 
-	 * @parameter default-value= "generate-simple-types.xsl"
-	 */
-	private String xslFile = null;
+        /**
+         * The XSL stylesheet file.
+         * 
+         * @parameter default-value= "generate-simple-types.xsl"
+         */
+        private String xslFile = null;
 
-	/**
-	 * An include pattern for the simple type definition files.
-	 * 
-	 * @parameter default-value=".type.xml"
-	 */
-	private String includePattern;
+        /**
+         * An include pattern for the simple type definition files.
+         * 
+         * @parameter default-value=".type.xml"
+         */
+        private String includePattern;
 
-	/**
-	 * The source folder with the simple type XML definitions.
-	 * 
-	 * @parameter
-	 */
-	private File sourceDirectory;
+        /**
+         * The source folder with the simple type XML definitions.
+         * 
+         * @parameter
+         */
+        private File sourceDirectory;
 
-	/**
-	 * The destination folder where the generated code will be written to.
-	 * 
-	 * @parameter
-	 */
-	private File destinationDirectory;
+        /**
+         * The destination folder where the generated code will be written to.
+         * 
+         * @parameter
+         */
+        private File destinationDirectory;
 
-	// TODO:
-	// http://code.hammerpig.com/search-for-files-in-directory-using-wildcards-in-java.html
-	public void execute() throws MojoExecutionException {
-		if (!destinationDirectory.exists())
-		{
-			destinationDirectory.mkdirs();
-		}
-		List<File> files = findFiles(sourceDirectory, includePattern);
-		for (File file : files) {
-			String log = file.getName() + ".log";
-			XsltBase.transform(file, xslFile, destinationDirectory, new File(project
-					.getBuild().getDirectory(), log), false);
-		}
-	}
+        // TODO:
+        // http://code.hammerpig.com/search-for-files-in-directory-using-wildcards-in-java.html
+        public void execute() throws MojoExecutionException {
+                if (!destinationDirectory.exists())
+                {
+                        destinationDirectory.mkdirs();
+                }
+                List<File> files = findFiles(sourceDirectory, includePattern);
+                for (File file : files) {
+                        String log = file.getName() + ".log";
+                        XsltBase.transform(file, xslFile, destinationDirectory, new File(project
+                                        .getBuild().getDirectory(), log), false);
+                }
+                project.addCompileSourceRoot(destinationDirectory.getAbsolutePath());
+        }
 
-	private List<File> findFiles(File dir, String pattern) {
-		List<File> files = new ArrayList<File>();
-		for (File file : dir.listFiles()) {
-			if (file.getName().endsWith((pattern))) {
-				files.add(file);
-			}
-		}
-		return files;
-	}
+        private List<File> findFiles(File dir, String pattern) {
+                List<File> files = new ArrayList<File>();
+                for (File file : dir.listFiles()) {
+                        if (file.getName().endsWith((pattern))) {
+                                files.add(file);
+                        }
+                }
+                return files;
+        }
 }
